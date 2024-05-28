e8f2ffc05597a9973a61ee7d0c743c0a5712e47e
Rasim <90181726+rasimishere@users.noreply.github.com>
2024-05-28 13:58:00 +0200

    Added main function to get the number of points from the user
    the main function to find out how many points will be created from the user.

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ class Point:
 
     def __repr__(self):
         return f"Point(x={self.x}, y={self.y})"
+
+def main():
+    num_points = int(input("how many points need to be created: "))
+
+if __name__ == "__main__":
+    main()
